ae466f9679072349651a7e96b8f15c7027c08463
david-uad <2103866@uad.ac.uk>
2023-03-06 19:04:52 +0000

    Added skeleton member functions for update, init, and constructor/destructors to GameObject{}

--- build/vs2017/GameObject.cpp
+++ build/vs2017/GameObject.cpp
@@ -1,3 +1,21 @@
 #include "GameObject.h"
 
-// test
+GameObject::GameObject()
+{
+
+}
+
+GameObject::~GameObject()
+{
+
+}
+
+void GameObject::Init()
+{
+	
+}
+
+void GameObject::Update()
+{
+
+}
--- build/vs2017/GameObject.h
+++ build/vs2017/GameObject.h
@@ -1,7 +1,10 @@
 #pragma once
 class GameObject
 {
+	GameObject();
+	~GameObject();
 
-	// test
+	void Init();
+	void Update();
 };
 
