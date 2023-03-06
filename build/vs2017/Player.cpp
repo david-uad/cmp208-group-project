a5420c85a8ae7518d4fa8f96b5f1b1e31f8068cd
alexjaycochrane <2103334@uad.ac.uk>
2023-03-06 19:44:06 +0000

    Added the basic elements of the Player class

--- build/vs2017/Player.cpp
+++ build/vs2017/Player.cpp
@@ -1,3 +1,47 @@
 #include "Player.h"
 
-// code goes here
+Player::Player()
+{
+	// set default values
+	health = 100.0;
+}
+
+Player::~Player()
+{
+
+}
+
+void Player::setKeyDown(unsigned char key)
+{
+	if (key >= 0)
+	{
+		keys[key] = true;
+	}
+}
+
+void Player::setKeyUp(unsigned char key)
+{
+	if (key >= 0)
+	{
+		keys[key] = false;
+	}
+}
+
+bool Player::isKeyDown(int key)
+{
+	if (key >= 0)
+	{
+		return keys[key];
+	}
+	return false;
+}
+
+float Player::getHealth()
+{
+	return health;
+}
+
+void Player::setHealth(float h)
+{
+	health = h;
+}
--- build/vs2017/Player.h
+++ build/vs2017/Player.h
@@ -1,9 +1,24 @@
 #pragma once
 #include "GameObject.h"
-class Player :
-    public GameObject
+class Player : public GameObject
 {
 
-    // code goes here
-};
+    float health;
 
+public:
+    Player();
+    ~Player();
+
+    // Getters and setters for keys
+    void setKeyDown(unsigned char key);
+    void setKeyUp(unsigned char key);
+    bool isKeyDown(int key);
+
+    // Getters and setters for player stats, etc.
+    float getHealth();
+    void setHealth(float h);
+
+private:
+    // Boolean array, element per key
+    bool keys[256]{ false };
+};
