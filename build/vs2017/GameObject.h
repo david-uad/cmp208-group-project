5553f1e2e8d8f8bfb579c7996c734f742f4d11bd
david-uad <2103866@uad.ac.uk>
2023-03-06 19:12:34 +0000

    Added uuseful includes and MeshInstance inheritance to GameObject

--- build/vs2017/GameObject.h
+++ build/vs2017/GameObject.h
@@ -1,10 +1,20 @@
+#include "box2d/box2d.h"
+#include "primitive_builder.h"
+#include <graphics/renderer_3d.h>
+#include <maths/math_utils.h>
+#include "graphics\mesh_instance.h"
+
 #pragma once
-class GameObject
+class GameObject : gef::MeshInstance
 {
+public:
 	GameObject();
 	~GameObject();
 
 	void Init();
 	void Update();
+
+private:
+	b2World* world = nullptr;
 };
 
